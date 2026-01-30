a416584ce36da20492a38beaf55362eca309b5e4
Vindhyesh Pandey <vindhyesh.pandey@radcom.com>
2026-01-30 16:48:50 +0530

    fix: remove submodule, make Testing_repo normal folder

---
+++ updated git code/Testing_repo/Parent/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers{
+    azurerm = {
+source = "hashicorp/azurerm"
+version = "4.58.0"
+
+    }
+  }
+}
+
+provider "azurerm" {
+    features {}
+    subscription_id = "1234567890"
+  
+}
